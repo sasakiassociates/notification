fb59b5b2b0017010c356cea6ab28c13819e405c9
Eric Youngberg <eyoungberg@sasaki.com>
2022-06-02 13:23:13 -0400

    Create singleton for mountint

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,21 @@
 
 export default class Notification {
 
+    private static drawer?: HTMLDivElement;
+
     constructor()  {
+        if (!Notification.drawer) {
+            Notification.mount();
+        }
+    }
+
+    private static mount() {
+        const drawer = document.createElement('div');
+        drawer.className = 'Notification__Drawer';
+
+        document.querySelector('body').appendChild(drawer);
+
+        Notification.drawer = drawer;
     }
 
 };
