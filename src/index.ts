f23c9303187221fb797fd77cd3a0e0bb5f9b1bb4
sdpy695uw325fyuv <sdpy695uw325fyuv@gmail.com>
2022-07-26 13:24:42 -0400

    push notifications not pushing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,67 @@
 
 
 import './index.scss';
+// import './service.js';
+
+
+
+/**
+ * Push Notification
+ * 
+ */
+
+
+
+// main(); we will not call main in the beginning.
+
+//         registerServiceWorker().then(function (registration) {
+//             let serviceWorker;
+//             if (registration.installing) {
+//                 console.log('installing')
+//                 serviceWorker = registration.installing;
+//                 document.querySelector('#kind').textContent = 'installing';
+//             } else if (registration.waiting) {
+//                 console.log('waiting')
+//                 serviceWorker = registration.waiting;
+//                 document.querySelector('#kind').textContent = 'waiting';
+//             } else if (registration.active) {
+//                 console.log('active')
+//                 serviceWorker = registration.active;
+//                 document.querySelector('#kind').textContent = 'active';
+//             }
+//             if (serviceWorker) {
+//                 // logState(serviceWorker.state);
+//                 serviceWorker.addEventListener('statechange', function (e) {
+//                     // logState(e.target.state);
+//                 });
+//             }
+//             console.log('registered')
+//         }).catch (function (error) {
+//             // Something went wrong during registration. The service-worker.js file
+//             // might be unavailable or contain a syntax error.
+//         });
+//     } else {
+//         // The current browser doesn't support service workers.
+//         // Perhaps it is too old or we are not in a Secure Context.
+//         throw new Error('No Service Worker support!')
+//     }
+// }
+
+    //If `serviceWorker` is registered and ready
+    // navigator.serviceWorker.ready
+    // .then(function (registration) {
+    //   registration.pushManager.getSubscription()
+    //   .catch(function (error) {
+    //     console.error('Error occurred while enabling push ', error);
+    //   });
+    // });
+    // console.log('permission granted')
+
+  
+
+/**
+ * Notification
+ */
 
 
 export type NotificationGlobals = {
@@ -24,8 +85,9 @@ export type NotificationConfig = NotificationGlobals & {
 
 
 export default class Notification {
-
+    //@ts-ignor
     public readonly args: NotificationArgs = { message: '' };
+    //
     public readonly element: HTMLDivElement;
 
     private _player: ReturnType<typeof setTimeout>;
@@ -51,7 +113,11 @@ export default class Notification {
             this.play();
         }
         else {
-            el.innerText = args.message;
+            console.log('args', args)
+            console.log('args.message', args.message)
+            if (args.message != null) {
+                el.innerText = args.message;
+            }
             
             if (args.className) {
                 el.className += ` ${args.className}`;
@@ -64,21 +130,57 @@ export default class Notification {
             this.args = args;
             
             //individual attributes of notification should override general configuration of class
-            
 
             if (args.defer !== undefined){
                 if (args.defer !== true){
                     this.play()
                 }
-                else{
-                }
             }
             else if (Notification.defer !== true){
                 this.play()
             }
-            else{
-            }
+          
         }
+        
+    }
+
+      
+    static check = () => {
+        if (!('serviceWorker' in navigator)) {
+            throw new Error('No service worker support!')
+        }
+        if (!('PushManager' in window)) {
+            throw new Error('No Push API Support!')
+        }
+    }
+
+    static registerServiceWorker = async () => {
+        const swRegistration = await navigator.serviceWorker.register('./service.js'); //notice the file name
+        return swRegistration
+    }
+
+    static requestNotificationPermission = async () => {
+        console.log('requesting')
+        const permission = await window.Notification.requestPermission()
+        // value of permission can be 'granted', 'default', 'denied'
+        // granted: user has accepted the request
+        // default: user has dismissed the notification permission popup by clicking on x
+        // denied: user has denied the request.
+        if (permission !== 'granted') {
+        throw new Error('Permission not granted for Notification')
+        }
+        console.log('requested')
+    }
+
+    static showLocalNotification = (title, body, swRegistration) => {
+        console.log('aaaa')
+        const options = {
+            body,
+            // here you can add more properties like icon, image, vibrate, etc.
+        };
+        console.log("REGISTRATION" , swRegistration)
+        console.log("aaaaa" , swRegistration.showNotification(title, options))
+        swRegistration.showNotification(title, options);
     }
 
     public finish() {
@@ -169,3 +271,5 @@ export default class Notification {
     }
 
 };
+
+
