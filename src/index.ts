9e762a8b3317a6dfde7cf08fc0a218ddd801c510
Eric Youngberg <eyoungberg@sasaki.com>
2022-08-19 13:13:34 -0400

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export type NotificationGlobals = {
     defer?: boolean;   // if true, notifications will not play upon instantiantion.
     duration?: number; // amount of time in milliseconds that a notification will show.
     flash?: boolean;   // if true, the latest notification will repeatedly flash in the tab title.
-    push?: boolean; //if true, the notification will appear as a push notification rather than locally on the webpage
+    push?: boolean;    // if true, the notification will appear as a push notification rather than locally on the webpage
 }
 
 export type NotificationArgs = NotificationGlobals & {
@@ -22,13 +22,14 @@ export type NotificationArgs = NotificationGlobals & {
 
 export type NotificationConfig = NotificationGlobals & {
     mount?: HTMLElement;
+    serviceWorker?: string;
 };
 
 
 export default class Notification {
-    //@ts-ignor
+
+    public finished: boolean = false;
     public readonly args: NotificationArgs = { message: '' };
-    //
     public readonly element: HTMLDivElement;
 
     private _player: ReturnType<typeof setTimeout>;
@@ -46,17 +47,15 @@ export default class Notification {
         el.className = "Notification";
 
         el.addEventListener('mouseenter', () => clearTimeout(this._player));
-        el.addEventListener('mouseleave', () => this._play());
+        el.addEventListener('mouseleave', () => !this.finished && this._play());
         el.addEventListener('click', () => this.finish());
         
         
         if (typeof args === 'string') {
             el.innerText = args;
             this.play();
-            
         }
         else {
-   
             if (args.message != null) {
                 el.innerText = args.message;
             }
@@ -71,44 +70,20 @@ export default class Notification {
 
             this.args = args;
             
-            //individual attributes of notification should override general configuration of class
-           
             if (Notification.defer !== undefined){
-                    if (args.defer !== true){
-                        this.play()
-                    }
+                if (args.defer !== true){
+                    this.play()
+                }
             }
             else if (Notification.defer !== true){
                 this.play()
             }
         }
     }
-
-    static registerServiceWorker = async () => {
-        const swRegistration = await navigator.serviceWorker.register('./service.js'); //notice the file name
-        return swRegistration
-    }
-
-
-    static requestNotificationPermission = async () => {
-        const permission = await window.Notification.requestPermission()
-        return (permission === 'granted')
-    }
-
-    static showPushNotification = async (title: string, body: string) => {
-        const options = {
-            title,
-            body,
-        };
-      
-        if(await Notification.requestNotificationPermission()) { 
-            navigator.serviceWorker.ready.then(async function(registration) {
-                registration.showNotification(title, options) 
-        }).then()}
-
-    }
   
     public finish() {
+        this.finished = true;
+
         clearTimeout(this._player);
         clearInterval(this._flasher);
 
@@ -116,28 +91,26 @@ export default class Notification {
 
         this._player = setTimeout(() => {
             this._drawer.removeChild(this.element);
-        }, 1000);
+        }, 500);
     }
 
     public play() {
-        //if the attributes of the notification class and the inividual notification align, play the notification locally 
-        if(!document.hidden && (this.args.push === false || (this.args.push === undefined && Notification.push !== true)) ){
-            Notification._current = this; 
-            this._drawer.appendChild(this.element);
+        Notification._current = this; 
+        this._drawer.appendChild(this.element);
 
-            if (this.args.flash === true || Notification.flash) {
-                this._flash();
-            }
-            this._play();
-        }
-        //if the attributes align, make it a push notification 
-        else {
-            Notification.showPushNotification("Sasaki", this.args.message)
+        if (this.args.flash === true || Notification.flash) {
+            this._flash();
         }
 
+        if (!(!document.hidden && (this.args.push === false || (this.args.push === undefined && Notification.push !== true)))) {
+            Notification._showPushNotification(document.title, this.args.message);
+        }
+
+        this._play();
     }
 
     private _play() {
+        this.finished = false;
         clearTimeout(this._player);
 
         this.element.classList.remove('finished');
@@ -177,20 +150,22 @@ export default class Notification {
     public static duration: number = 3000;
     public static flash: boolean = false;
     public static push: boolean = false;
+    public static serviceWorker: string = './notification.js';
     private static _drawer?: HTMLDivElement;
     private static _current?: Notification;
+    private static _serviceWorker?: any;
 
     public static configure(config: NotificationConfig) {
-
         if (config.mount && config.mount !== Notification.mount) {
             Notification.mount = config.mount;
             Notification._createDrawer();
         }
+
         Notification.defer = config.defer !== undefined ? config.defer : Notification.defer;
         Notification.duration = config.duration !== undefined ? config.duration : Notification.duration;
         Notification.flash = config.flash !== undefined ? config.flash : Notification.flash;
         Notification.push = config.push !== undefined ? config.push: Notification.push;
-
+        Notification.serviceWorker = config.serviceWorker !== undefined ? config.serviceWorker : Notification.serviceWorker;
     }
 
     private static _createDrawer() {
@@ -203,6 +178,31 @@ export default class Notification {
 
         Notification.mount.appendChild(drawer);
         Notification._drawer = drawer;
+    }
+
+    private static async _registerServiceWorker() {
+        return await navigator.serviceWorker.register(Notification.serviceWorker);
+    }
+
+    private static async _requestNotificationPermission() {
+        return (await window.Notification.requestPermission()) === 'granted';
+    }
+
+    private static async _showPushNotification(title: string, body: string) {
+        const options = {
+            title,
+            body,
+        };
+
+        if (!Notification._serviceWorker) {
+            Notification._serviceWorker = await Notification._registerServiceWorker();
+        }
+      
+        if(await Notification._requestNotificationPermission()) { 
+            navigator.serviceWorker.ready.then(async function(registration) {
+                registration.showNotification(title, options) 
+            });
+        }
     }
 
 };
